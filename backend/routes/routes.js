c4248912747232e6b4f0c0755cab75ecbb915b4d
fer2583 <fernandoarielmartinez83@gmail.com>
2023-07-11 00:24:41 -0300

    actualizar

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const buttonController = require('../controllers/buttonController');
 
 // Ruta crear Boton
-router.post('/buttons', buttonController.createButton);
+router.post('/buttons', buttonController.crearBoton);
 
 // Ruta registrar Click en un botón
-router.post('/buttons/:id/clicks', buttonController.registerClick);
+router.post('/buttons/:id/clicks', buttonController.cantidadClick);
 
 // Ruta para todos los botones
-router.get('/buttons', buttonController.getAllButtons);
+router.get('/buttons', buttonController.obtenerBotones);
 
 // Ruta para un botón específico
-router.get('/buttons/:id', buttonController.getButton);
+router.get('/buttons/:id', buttonController.obtenerUnBoton);
 
 // Ruta para eliminar un botón
-router.delete('/buttons/:id', buttonController.deleteButton); 
+router.delete('/buttons/:id', buttonController.borrarBoton); 
 
 module.exports = router;
 
